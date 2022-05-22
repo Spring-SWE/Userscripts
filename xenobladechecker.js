031bea9839d42dcf1994979d6275e72611a5c62d
Spring <ashyiscool2@gmail.com>
2022-05-22 19:19:40 +0000

    Update xenobladechecker.js

--- xenobladechecker.js
+++ xenobladechecker.js
@@ -3,13 +3,17 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @author       Tony
-// @match        https://www.nintendo.com/store/products/xenoblade-chronicles-3-special-edition-switch/
+// @match https://www.nintendo.com/store/products/xenoblade-chronicles-3-special-edition-switch/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=hibbard.eu
 // @grant        none
-// @require http://code.jquery.com/jquery-latest.js
+// @require      http://code.jquery.com/jquery-latest.js
+// @run-at document-idle
 // ==/UserScript==
 (function() {
     'use strict';
+
+    let statusText = '';
+    let statusHTML = '';
 
     (async () => {
         // create and show the notification
@@ -37,7 +41,10 @@
             if (granted === true) {
                 $.get("https://graph.nintendo.com/?operationName=ProductDetail&variables=%7B%22slug%22%3A%22xenoblade-chronicles-3-special-edition-switch%22%2C%22locale%22%3A%22en_US%22%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%22c6211f0f1ee9c21463b70c891f99476678bc137b0d37374fedabb514e621401f%22%7D%7D", function(data) {
                     if (data.data.products[0].availability[0] === 'Coming soon') {
-                        console.log('product still TBD....will check again in 30 seconds.')
+                        statusText = 'Script is currently running. Please leave page open.';
+                        statusHTML = $("<div style='padding:1rem; color:#055160;background-color: #cff4fc;border-color: #b6effb;}'><span class='Textstyles__StyledSpan-sc-14mne8t-3 hNqrMw StoreNotificationstyles__Text-sc-1g8xmtg-5 Pqbsw'>" + statusText + "</span></div>");
+                        $('.StoreNotificationstyles__Wrapper-sc-1g8xmtg-0').empty().append(statusHTML);
+
                         setTimeout(function() {
                             checkAvailabilityStatus();
                         }, 30000);
@@ -47,8 +54,9 @@
                     }
                 })
             } else {
-                alert('This script requires Desktop Notifications to be granted!');
-                Notification.requestPermission();
+                statusText = 'This script REQUIRES notifications to be turned on, please do so and refresh,';
+                statusHTML = $("<div style='padding:1rem; color:#842029;background-color: #f8d7da;border-color: #b6effb;}'><span class='Textstyles__StyledSpan-sc-14mne8t-3 hNqrMw StoreNotificationstyles__Text-sc-1g8xmtg-5 Pqbsw'>" + statusText + "</span></div>");
+                $('.StoreNotificationstyles__Wrapper-sc-1g8xmtg-0').empty().append(statusHTML);
             }
 
         }
